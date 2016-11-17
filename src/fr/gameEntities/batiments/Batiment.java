9c8ae351b9b68da375e9a928129f7f4986968e5f
Llemilian <robin-andco@hotmail.fr>
2016-11-17 10:16:59 +0100

    before pull

--- src/fr/gameEntities/batiments/Batiment.java
+++ src/fr/gameEntities/batiments/Batiment.java
@@ -1,45 +1,50 @@
 package fr.gameEntities.batiments;
 
 import fr.entities.BackupConstruction;
+import fr.entities.Construction;
 import fr.gameEntities.AbstractGameEntity;
 import fr.gameEntities.indicateurs.Budget;
+import fr.gameEntities.indicateurs.Population;
 import fr.interfaces.IBatiment;
 import fr.interfaces.IEntity;
 
 public abstract class Batiment<T> extends AbstractGameEntity<T> implements IBatiment{
-	
-	// class de l'entities
 
-	protected int nbCadre;				// Pour leur permettre d'evoluer, les nombres de poste seront multiplies par 10
-	protected int nbSalarie;
-//	protected int prixBatiment;
-//	private int nbHautDiplome;
-//	private int nbSpecialiste;
-	
-	
 	public Batiment (){
 	}
-	
-	public Batiment (int niv){
+	public Batiment(int baseS, int baseC, int baseR, int att){
+		this();
+		this.getEntity()
+		((Construction) this.entity).setBaseSalarie(baseS);
+		((Construction) this.entity).setBaseCadre(baseC);
+		((Construction) this.entity).setModRisque(baseR);
+		((Construction) this.entity).setBaseAttractivite(att);
+		this.prisePostes();
 	}
 	
-	public int coutPers(){
-		int p = this.nbCadre*Budget.getSalaireCadre()+
-				this.nbSalarie*Budget.getSalaireStandard()/*+
-				this.nbHautDiplome*Budget.getSalaireHautDiplome()+
-				this.nbSpecialiste*Budget.getSalaireSpecialiste()*/;
-		return p;
+	public void ameliore(){
+		((T) this.entity).setNbSalarie(6);
 	}
 	
-	public void detruire(){
+	public int getPostePourvu(){
+		return this.postePourvu;
 	}
-	public void ameliorer(){
+	public void prisePostes(){
+		int pEmbauche = Population.nbIndiv(Budget.getAgeTravail(), Budget.getAgeRetraite())-getPostesPourvus();
+		if (pEmbauche <= 0)
+			this.postePourvu = 0;
+		else{
+			if (pEmbauche > (this.entity.getNbSalarie()+this.nbCadre)/10)
+				this.postePourvu = (this.nbSalarie+this.nbCadre)/10;
+			else
+				this.postePourvu = pEmbauche;
+		}
 	}
-	public int getNbCadre() {
-		return nbCadre/10;
+	public void ajoutPoste(){
+		int pEmbauche = Population.nbIndiv(Budget.getAgeTravail(), Budget.getAgeRetraite())-getPostesPourvus();
+		if (pEmbauche > (this.nbSalarie+this.nbCadre)/10)
+			this.postePourvu = (this.nbSalarie+this.nbCadre)/10;
+		else
+			this.postePourvu += pEmbauche;
 	}
-	public int getNbSalarie() {
-		return nbSalarie/10;
-	}
-
 }
--- src/fr/gameEntities/indicateurs/Population.java
+++ src/fr/gameEntities/indicateurs/Population.java
@@ -1,44 +1,40 @@
-package fr.gameEntities.indicateurs;
+package fr.indicateur;
 
 public class Population extends Thread {
-	private int[][] popTab = new int[130][12];
-	private int fertilite = 29;		// fertilite x 10 pour rester en int
+	private static int[][] popTab = new int[130][12];
+	private static int fertilite = 29;		// fertilite x 10 pour rester en int
+	private static int attractivite = 0;
 	
 // Entree	: nombre total de la population
 // Fonction : reparti aleatoirement la population dans les differentes tranches d'age
 // Remarque : Version basique a detailler
 // Verifiee : oui
 	public Population (int a){
-		int i;
 		int adulte = a/5*2;
 		int b = a-adulte;
-		this.ajoutPopulation(adulte, 18, 42);
-		this.ajoutPopulation(b, 0, 80);
+		ajoutPopulation(adulte, 18, 42);
+		ajoutPopulation(b, 0, 80);
 	}	
-	public void ajoutPopulation (int a, int agemin, int agemax){
+	public static void ajoutPopulation (int a, int agemin, int agemax){
 		int i, col, lig;
 		for (i=a; i>0; i--){
 			int random = (int)(Math.random() * (agemax-agemin)*12); // permet de mettre al�atoirement la population dans la tranche d'age choisie.
 			col = random/12;
 			lig = random%12;
-			//System.out.println("col = " + col + " lig = " + lig);
-			this.popTab[col][lig] ++;
+			popTab[col][lig] ++;
 		}
 	}
-	public void retraitPopulation (int a, int agemin, int agemax){
-		//System.out.println("entree " + a + " " + agemin);
+	public static void retraitPopulation (int a, int agemin, int agemax){
 		int col, lig;
 		while(a > 0){
 			int random = (int)(Math.random() * (agemax-agemin+1)*12); // permet de retirer al�atoirement la population dans la tranche d'age choisie.
 			col = random/12;
 			lig = random%12;
-			//System.out.println("col = " + col + " lig = " + lig);
-			if (this.popTab[col][lig] > 0){
-				this.popTab[col][lig] --;
+			if (popTab[col][lig] > 0){
+				popTab[col][lig] --;
 				a--;
 			}
 		}
-		//System.out.println("sortie");
 	}
 
 // Entree	: le nombre d'individus a evaluer. L'age des sujets
@@ -49,7 +45,7 @@ public class Population extends Thread {
 //	  		  4 La fonction est public, peut-etre la rendre private
 //			  5 implementer comme facteur de mortalite : la quantite de population (et le taux de criminalite) 
 // Verifiee	: Oui
-	public int morts(int nbIndiv, int age){
+	public static int morts(int nbIndiv, int age){
 		int nbMort;
 		if (age < 3){
 			nbMort = (age-3)*(age-3);
@@ -68,6 +64,9 @@ public class Population extends Thread {
 		return nbMort;
 	}
 
+	public static void fertilite(){
+		fertilite = (int)(30+Math.sqrt(attractivite/4)-20);
+	}
 // Entree	: / - La fonction regroupe toutes les naissances de la population et les regroupe dans la case des nouveaux nes
 // Fonction : Definit le nombre de naissance en fonction du nombre de personne en age de reproduction de la cite et du taux de fertilite
 // Remarque : 1 - Le taux de fertilite est multiple par 10 pour rester en INT
@@ -75,9 +74,9 @@ public class Population extends Thread {
 //			  3 - La fonction renvoie un entier, utile pour le developpement, possibilite de ne rien renvoyer
 //			  4 - La fonction est public, peut-etre la rendre private
 // Verifiee :
-	public int naissances (){
+	public static int naissances (){
 		int x, y, n = 0;
-		int num = nbIndiv(17, 42) * this.fertilite;
+		int num = nbIndiv(17, 42) * fertilite;
 		int denom = 2*12*25*10;							// Nb indiviu par couple * nb mois * nb annee * equilibrateur fertilite
 		while (num > denom){
 			num -= denom;
@@ -95,31 +94,34 @@ public class Population extends Thread {
 // Fonction : Renvoie le nombre d'individu de la tranche d'age demandee
 // Remarque :
 // Verifiee : Oui
-	public int nbIndiv (int min, int max){
+	public static int nbIndiv (int min, int max){
 		int i, j, nb = 0;
 		for (i = min; i <= max; i++){
 			for (j=0; j < 12; j++)
-				nb += this.popTab[i][j];
+				nb += popTab[i][j];
 		}
 		return nb;
+	}
+	public static int nbIndiv(){
+		return nbIndiv(0, 129);
 	}
 
 // Entree	: / Agit directement sur toute la population
 // Fonction	: Effectue les differentes operations de naissance, de mort et de vieillissement de la population (fait appel aux fonctions 'naissance' et 'mort'
 // Remarque	: Fonction centrale de l'evolution de la population.
 // Verifiee	: Oui
-	public int vieillissementM (){
+	public static int vieillissementM (){
 		int i, j, nbN;
-		nbN = this.naissances();
+		nbN = naissances();
 		for (i = 129; i >= 0; i--){
 			for (j = 11; j >= 0; j--){
 				if (i == 0 && j == 0){
-					this.popTab[i][j] = nbN;
+					popTab[i][j] = nbN;
 				} else {
 					if (j == 0){
-						this.popTab[i][j] = this.popTab[i-1][11]-morts(this.popTab[i-1][11], i);//vieillissement
+						popTab[i][j] = popTab[i-1][11]-morts(popTab[i-1][11], i);//vieillissement
 					} else {
-						this.popTab[i][j] = this.popTab[i][j-1]-morts(this.popTab[i][j-1], i); //vieillissement
+						popTab[i][j] = popTab[i][j-1]-morts(popTab[i][j-1], i); //vieillissement
 					}
 				}
 			}
@@ -131,12 +133,12 @@ public class Population extends Thread {
 // Fonction : Affiche tout le tableau
 // Remarque : L'affichage se fait a l'echelle annuelle
 // Verifiee : Oui
-	public void affichePop (){
+	public static void affichePop (){
 		int i, j, nb;
 		for (i = 0; i < 130; i++){
 			nb=0;
 			for (j=0; j < 12; j++)
-				nb += this.popTab[i][j];
+				nb += popTab[i][j];
 			System.out.println("Il y a " + nb + " individus de " + i + " an(s)");
 		}
 	}
@@ -145,7 +147,7 @@ public class Population extends Thread {
 // Fonction	: Affiche la tranche d'age souhaitee
 // Remarque	: L'affichage se fait a l'echelle annuelle
 // Verifiee	: Oui
-	public void affichePop (int min, int max){
+	public static void affichePop (int min, int max){
 		int i, j, nb;
 		if (min > max){
 			int r = min;
@@ -159,7 +161,7 @@ public class Population extends Thread {
 		for (i = min; i <= max; i++){
 			nb=0;
 			for (j=0; j < 12; j++)
-				nb += this.popTab[i][j];
+				nb += popTab[i][j];
 			System.out.println("Il y a " + nb + " individus de " + i + " an(s)");
 		}
 	}
@@ -168,45 +170,30 @@ public class Population extends Thread {
 // Fonction	: reparti les migrants dans des tranches d'age coherentes et appelle les fonction pour modifier le tableau de population
 // Remarque	: 
 // Verifiee	: Oui
-	public int flux (int a){
+	public static int flux (int a){
 		if (a>=0){
-			int adulte = a/5*2;
+			int adulte = a/5*2;	// oblige les nouveaux arrivant a etre coherent
 			int b = a-adulte;
-			this.ajoutPopulation(adulte, 18, 42);
-			this.ajoutPopulation(b, 0, 80);
+			ajoutPopulation(adulte, 18, 42);
+			ajoutPopulation(b, 0, 80);
 		} else {
 			//System.out.println(this.nbIndiv(18, 42));
-			if (-a > this.nbIndiv(0, 80)){
-				a = -this.nbIndiv(0, 80);
+			if (-a > nbIndiv(0, 80)){
+				a = -nbIndiv(0, 80);
 			}
-			int adulte = a/5*2;
-			if (-adulte > this.nbIndiv(18, 42))
-				adulte = this.nbIndiv(18, 42);
+			int adulte = a/5*2;	// Les departs ne se font avec des adultes
+			if (-adulte > nbIndiv(18, 42))
+				adulte = nbIndiv(18, 42);
 			int b = a+adulte;
-			this.retraitPopulation(-adulte, 18, 42);
-			this.retraitPopulation(-b, 0, 80);
+			retraitPopulation(-adulte, 18, 42);
+			retraitPopulation(-b, 0, 80);
 		}
 		return a;
 	}
+	public static int migration(){
+		return (int)((Math.random()*nbIndiv()/5+attractivite)-nbIndiv()/10);
+	}
+	public static void modAttraction(int mod){
+		attractivite += mod;
+	}
 }
-
-// A venir : l'utilisation des Thread (sleep, essentiellement) pour lancer une simulation a partir de quelques parametres initiaux. 
-
-
-/*package tutorialthread.example01;
-
-public class Unthread extends thread{
-  public void run() {
-    long start = System.currentTimeMillis();
-    // boucle tant que la dur�e de vie du thread est < � 5 secondes
-    while( System.currentTimeMillis() < ( start + (1000 * 5))) {
-      // traitement
-      System.out.println("Ligne affich�e par le thread");
-      try {
-        // pause
-        thread.sleep(500);
-      }
-      catch (InterruptedException ex) {}
-    }
-  }    
-}*/
