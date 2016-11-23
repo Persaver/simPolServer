a74c65080dbe63b91dc9fa996213710b4076eb38
NaokoD <ludovic.damprobe@gmail.com>
2016-11-23 15:52:16 +0100

    Save BackupConstruction Construction Categorie

--- src/fr/Dao/CategorieDAO.java
+++ src/fr/Dao/CategorieDAO.java
@@ -3,6 +3,7 @@ package fr.Dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +16,8 @@ public class CategorieDAO extends DAO<Categorie,Integer> {
 		Categorie categorie= null;
 		ResultSet result;
 		try {
-			PreparedStatement prepare = this.connect.prepareStatement("Select * From categorie where id = ?");
+			String sql ="Select * From categorie where id = ?";
+			PreparedStatement prepare = this.connect.prepareStatement(sql);
 			prepare.setInt(1, id);
 			result = prepare.executeQuery();
 			if(result != null){
@@ -30,10 +32,29 @@ public class CategorieDAO extends DAO<Categorie,Integer> {
 	}
 
 	@Override
-	public void save(Categorie element) {
-		// TODO Auto-generated method stub
+	public Categorie save(Categorie element) {
+		try {
+			String sql = "INSERT INTO categorie (libelle) VALUES (?)";
 
+			PreparedStatement statement = this.connect.prepareStatement( sql, Statement.RETURN_GENERATED_KEYS );
+			statement.setString(1, element.getLibelle());
+			statement.executeUpdate();
+			ResultSet generatedKeys = statement.getGeneratedKeys();
+			if (generatedKeys.first()) {
+				element.setId(generatedKeys.getInt(1));
+			} else {
+				throw new SQLException("Creating message failed, no ID obtained.");
+			}
+			statement.close();
+			generatedKeys.close();
+			return element;
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+		return null;
 	}
+
+	// TODO Auto-generated method stub
 
 	@Override
 	public void delete(Integer id) {
@@ -52,7 +73,8 @@ public class CategorieDAO extends DAO<Categorie,Integer> {
 		ResultSet result;
 		List<Categorie> categories = new ArrayList<Categorie>();
 		try {
-			result = this.connect.createStatement().executeQuery("Select * from categorie");
+			String sql="Select * from categorie";
+			result = this.connect.createStatement().executeQuery(sql);
 			while(result.next()){
 				Categorie categorie = new Categorie(result.getInt("categorie.id"), result.getString("libelle"));
 				categories.add(categorie);
