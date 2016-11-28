5b80cdef059f4ac81235386419b5d6f99a88a1a3
Geoffrey <gmartin.pro@gmail.com>
2016-11-28 21:03:37 +0100

    les valeurs sont renseignées dans l'insert.sql et enlevées du simpol.sql, attention à la table backup

--- insert.sql
+++ insert.sql
@@ -1,11 +1,69 @@
 use simpol;
 
-
 INSERT INTO user (login,password)
 VALUES ('saver','saver'),
       ('ludo','ludo'),
       ('geoffrey','geoffrey'),
       ('robin','robin');
-
+	  
 INSERT INTO backup(user)
 VALUES ('1'),('2'),('3'),('4');
+
+INSERT INTO `budget` (`id`, `ageTravail`, `ageRetraite`, `chargeSalariale`, `chargeCadre`, `salaireStandard`, `salaireCadre`, `nbSalaries`, `nbCadres`, `backup`) VALUES
+(1, 14, 70, 0, 0, 1000, 1500, 10, 1, 0),
+(2, 18, 65, 0, 0, 1200, 2000, 6, 1, 0),
+(3, 16, 65, 0, 0, 1200, 2250, 15, 2, 0);
+
+INSERT INTO `categorie` (`id`, `libelle`) VALUES
+(1, 'education'),
+(2, 'securite'),
+(3, 'economie'),
+(4, 'distraction'),
+(5, 'sante');
+
+INSERT INTO `construction` (`id`, `designation`, `w`, `h`, `price`, `baseSalarie`, `baseCadre`, `baseRisque`, `baseAttractivite`, `modSalarie`, `modCadre`, `modRisque`, `modAttractivite`, `specificite`, `categorie`) VALUES
+(1, 'maison', 1, 1, '80000.00', 0, 0, 0, 0, 0, 0, 0, 0, '', 3),
+(2, 'musee', 1, 1, '99999.99', 3, 1, 0, 0, 0, 0, 0, 0, '', 4),
+(3, 'restaurant', 1, 1, '99999.99', 3, 1, 0, 0, 0, 0, 0, 0, '', 4),
+(4, 'bureau', 1, 1, '99999.99', 10, 1, 0, 0, 0, 0, 0, 0, '', 3),
+(5, 'commissariat', 1, 1, '99999.99', 10, 2, 0, 0, 0, 0, 0, 0, '', 2),
+(6, 'hopital', 1, 1, '99999.99', 15, 3, 0, 0, 0, 0, 0, 0, '', 5),
+(7, 'entrepot', 1, 1, '75000.00', 10, 2, 0, 0, 0, 0, 0, 0, '', 3),
+(8, 'eglise', 1, 1, '99999.99', 1, 1, 0, 0, 0, 0, 0, 0, '', 4),
+(9, 'night_club', 1, 1, '50000.00', 1, 3, 0, 0, 0, 0, 0, 0, '', 4),
+(10, 'temple', 1, 1, '99999.99', 1, 1, 0, 0, 0, 0, 0, 0, '', 4),
+(11, 'temple_tower', 1, 1, '50000.00', 1, 1, 0, 0, 0, 0, 0, 0, '', 4),
+(12, 'ferme', 1, 1, '10000.00', 1, 2, 0, 0, 0, 0, 0, 0, '', 3),
+(13, 'Rescue_tower', 1, 1, '50000.00', 1, 5, 0, 0, 0, 0, 0, 0, '', 2),
+(14, 'Circus', 1, 1, '99999.99', 1, 10, 0, 0, 0, 0, 0, 0, '', 4),
+(15, 'Television', 1, 1, '99999.99', 3, 20, 0, 0, 0, 0, 0, 0, '', 3),
+(16, 'Salle_de_spectacle', 1, 1, '99999.99', 1, 8, 0, 0, 0, 0, 0, 0, '', 4),
+(17, 'Caserne', 1, 1, '75000.00', 1, 8, 0, 0, 0, 0, 0, 0, '', 2),
+(18, 'Tour_militaire', 1, 1, '75000.00', 1, 8, 0, 0, 0, 0, 0, 0, '', 2),
+(19, 'usine', 1, 1, '75000.00', 2, 20, 0, 0, 0, 0, 0, 0, '', 2),
+(20, 'universite', 1, 1, '99999.99', 2, 20, 0, 0, 0, 0, 0, 0, '', 1);
+
+INSERT INTO `criminalite` (`id`, `indicMineur`, `crimeMineur`, `indicMoyen`, `crimeMoyen`, `indicGrave`, `crimeGrave`, `indicTerrorisme`, `crimeTerroriste`, `backup`) VALUES
+(1, 1, 1, 1, 1, 1, 1, 1, 1, 0),
+(2, 2, 2, 2, 2, 2, 2, 2, 2, 0),
+(3, 3, 3, 3, 3, 3, 3, 3, 3, 0),
+(4, 4, 4, 4, 4, 4, 4, 4, 4, 0),
+(5, 5, 5, 5, 5, 5, 5, 5, 5, 0),
+(6, 6, 6, 6, 6, 6, 6, 6, 6, 0),
+(7, 7, 7, 7, 7, 7, 7, 7, 7, 0),
+(8, 8, 8, 8, 8, 8, 8, 8, 8, 0),
+(9, 9, 9, 9, 9, 9, 9, 9, 9, 0),
+(10, 10, 10, 10, 10, 10, 10, 10, 10, 0);
+
+INSERT INTO `population` (`id`, `fertilite`, `attractivite`, `popTab`, `backup`) VALUES
+(1, 1, 1, '1000', 0),
+(2, 2, 2, '15000', 0),
+(3, 3, 3, '25000', 0),
+(4, 4, 4, '40000', 0),
+(5, 5, 5, '50000', 0);
+
+INSERT INTO `user` (`id`, `login`, `password`) VALUES
+(1, 'Geoffrey', '0000'),
+(2, 'Saverio', '0000'),
+(3, 'Robin', '0000'),
+(4, 'Ludovic', '0000');
--- simpol.sql
+++ simpol.sql
@@ -6,32 +6,22 @@
 -- Généré le :  Sam 26 Novembre 2016 à 16:57
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
 --
 -- Base de données :  `simpol`
 --
-
+DROP DATABASE IF EXISTS simpol;
+CREATE DATABASE simpol DEFAULT CHARACTER SET utf8;
+USE simpol;
 -- --------------------------------------------------------
-
 --
 -- Structure de la table `backup_construction`
 --
-
-DROP DATABASE IF EXISTS simpol;
-CREATE DATABASE simpol DEFAULT CHARACTER SET utf8;
-
-USE simpol;
-
-
 CREATE TABLE IF NOT EXISTS `backup_construction` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `backup` int(4) NOT NULL,
@@ -48,24 +38,20 @@ CREATE TABLE IF NOT EXISTS `backup_construction` (
   PRIMARY KEY (`id`),
   KEY `construction` (`construction`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
 -- --------------------------------------------------------
 
-INSERT INTO backup_construction (backup,construction,x,y)
-VALUES (1,1,10,10),(1,2,10,10),(1,3,10,10);
-
-id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
-  date_last DATETIME DEFAULT CURRENT_TIMESTAMP,
-  nbj INT(5) NOT NULL DEFAULT 0,
-  user 
-  INSERT INTO backup (nbj,user)
-  VALUES(1,1);
-
+-- Table backup
+-- id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  -- date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
+  -- date_last DATETIME DEFAULT CURRENT_TIMESTAMP,
+  -- nbj INT(5) NOT NULL DEFAULT 0,
+  -- user 
+  -- INSERT INTO backup (nbj,user)
+  -- VALUES(1,1);
+  
 --
 -- Structure de la table `budget`
 --
-
 CREATE TABLE IF NOT EXISTS `budget` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `ageTravail` int(11) DEFAULT NULL,
@@ -81,20 +67,8 @@ CREATE TABLE IF NOT EXISTS `budget` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
--- Contenu de la table `budget`
---
-
-INSERT INTO `budget` (`id`, `ageTravail`, `ageRetraite`, `chargeSalariale`, `chargeCadre`, `salaireStandard`, `salaireCadre`, `nbSalaries`, `nbCadres`, `backup`) VALUES
-(1, 14, 70, 0, 0, 1000, 1500, 10, 1, 0),
-(2, 18, 65, 0, 0, 1200, 2000, 6, 1, 0),
-(3, 16, 65, 0, 0, 1200, 2250, 15, 2, 0);
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `categorie`
 --
-
 CREATE TABLE IF NOT EXISTS `categorie` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(30) NOT NULL,
@@ -102,22 +76,8 @@ CREATE TABLE IF NOT EXISTS `categorie` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
--- Contenu de la table `categorie`
---
-
-INSERT INTO `categorie` (`id`, `libelle`) VALUES
-(1, 'education'),
-(2, 'securite'),
-(3, 'economie'),
-(4, 'distraction'),
-(5, 'sante');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `construction`
 --
-
 CREATE TABLE IF NOT EXISTS `construction` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `designation` varchar(30) NOT NULL,
@@ -139,37 +99,8 @@ CREATE TABLE IF NOT EXISTS `construction` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
 
 --
--- Contenu de la table `construction`
---
-
-INSERT INTO `construction` (`id`, `designation`, `w`, `h`, `price`, `baseSalarie`, `baseCadre`, `baseRisque`, `baseAttractivite`, `modSalarie`, `modCadre`, `modRisque`, `modAttractivite`, `specificite`, `categorie`) VALUES
-(1, 'maison', 1, 1, '80000.00', 0, 0, 0, 0, 0, 0, 0, 0, '', 3),
-(2, 'musee', 1, 1, '99999.99', 3, 1, 0, 0, 0, 0, 0, 0, '', 4),
-(3, 'restaurant', 1, 1, '99999.99', 3, 1, 0, 0, 0, 0, 0, 0, '', 4),
-(4, 'bureau', 1, 1, '99999.99', 10, 1, 0, 0, 0, 0, 0, 0, '', 3),
-(5, 'commissariat', 1, 1, '99999.99', 10, 2, 0, 0, 0, 0, 0, 0, '', 2),
-(6, 'hopital', 1, 1, '99999.99', 15, 3, 0, 0, 0, 0, 0, 0, '', 5),
-(7, 'entrepot', 1, 1, '75000.00', 10, 2, 0, 0, 0, 0, 0, 0, '', 3),
-(8, 'eglise', 1, 1, '99999.99', 1, 1, 0, 0, 0, 0, 0, 0, '', 4),
-(9, 'night_club', 1, 1, '50000.00', 1, 3, 0, 0, 0, 0, 0, 0, '', 4),
-(10, 'temple', 1, 1, '99999.99', 1, 1, 0, 0, 0, 0, 0, 0, '', 4),
-(11, 'temple_tower', 1, 1, '50000.00', 1, 1, 0, 0, 0, 0, 0, 0, '', 4),
-(12, 'ferme', 1, 1, '10000.00', 1, 2, 0, 0, 0, 0, 0, 0, '', 3),
-(13, 'Rescue_tower', 1, 1, '50000.00', 1, 5, 0, 0, 0, 0, 0, 0, '', 2),
-(14, 'Circus', 1, 1, '99999.99', 1, 10, 0, 0, 0, 0, 0, 0, '', 4),
-(15, 'Television', 1, 1, '99999.99', 3, 20, 0, 0, 0, 0, 0, 0, '', 3),
-(16, 'Salle_de_spectacle', 1, 1, '99999.99', 1, 8, 0, 0, 0, 0, 0, 0, '', 4),
-(17, 'Caserne', 1, 1, '75000.00', 1, 8, 0, 0, 0, 0, 0, 0, '', 2),
-(18, 'Tour_militaire', 1, 1, '75000.00', 1, 8, 0, 0, 0, 0, 0, 0, '', 2),
-(19, 'usine', 1, 1, '75000.00', 2, 20, 0, 0, 0, 0, 0, 0, '', 2),
-(20, 'universite', 1, 1, '99999.99', 2, 20, 0, 0, 0, 0, 0, 0, '', 1);
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `criminalite`
 --
-
 CREATE TABLE IF NOT EXISTS `criminalite` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `indicMineur` int(11) DEFAULT NULL,
@@ -185,27 +116,8 @@ CREATE TABLE IF NOT EXISTS `criminalite` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
--- Contenu de la table `criminalite`
---
-
-INSERT INTO `criminalite` (`id`, `indicMineur`, `crimeMineur`, `indicMoyen`, `crimeMoyen`, `indicGrave`, `crimeGrave`, `indicTerrorisme`, `crimeTerroriste`, `backup`) VALUES
-(1, 1, 1, 1, 1, 1, 1, 1, 1, 0),
-(2, 2, 2, 2, 2, 2, 2, 2, 2, 0),
-(3, 3, 3, 3, 3, 3, 3, 3, 3, 0),
-(4, 4, 4, 4, 4, 4, 4, 4, 4, 0),
-(5, 5, 5, 5, 5, 5, 5, 5, 5, 0),
-(6, 6, 6, 6, 6, 6, 6, 6, 6, 0),
-(7, 7, 7, 7, 7, 7, 7, 7, 7, 0),
-(8, 8, 8, 8, 8, 8, 8, 8, 8, 0),
-(9, 9, 9, 9, 9, 9, 9, 9, 9, 0),
-(10, 10, 10, 10, 10, 10, 10, 10, 10, 0);
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `population`
 --
-
 CREATE TABLE IF NOT EXISTS `population` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `fertilite` int(11) DEFAULT '29',
@@ -216,22 +128,8 @@ CREATE TABLE IF NOT EXISTS `population` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
--- Contenu de la table `population`
---
-
-INSERT INTO `population` (`id`, `fertilite`, `attractivite`, `popTab`, `backup`) VALUES
-(1, 1, 1, '1000', 0),
-(2, 2, 2, '15000', 0),
-(3, 3, 3, '25000', 0),
-(4, 4, 4, '40000', 0),
-(5, 5, 5, '50000', 0);
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `user`
 --
-
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `login` varchar(30) NOT NULL,
@@ -239,32 +137,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
---
--- Contenu de la table `user`
---
-
-INSERT INTO `user` (`id`, `login`, `password`) VALUES
-(1, 'Geoffrey', '0000'),
-(2, 'Saverio', '0000'),
-(3, 'Robin', '0000'),
-(4, 'Ludovic', '0000');
-
---
--- Contraintes pour les tables exportées
---
-
---
 -- Contraintes pour la table `backup_construction`
 --
 ALTER TABLE `backup_construction`
   ADD CONSTRAINT `backup_construction_ibfk_1` FOREIGN KEY (`construction`) REFERENCES `construction` (`id`);
-
 --
 -- Contraintes pour la table `construction`
 --
 ALTER TABLE `construction`
   ADD CONSTRAINT `construction_ibfk_1` FOREIGN KEY (`categorie`) REFERENCES `categorie` (`id`);
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
