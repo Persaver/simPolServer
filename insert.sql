d2b1aaf4935545e14685f8ee7a6d69a0289827ea
Saverio Perri <saverio.perri@mail.com>
2016-12-01 15:02:50 +0100

    modif sql

--- insert.sql
+++ insert.sql
@@ -4,7 +4,7 @@ INSERT INTO user (`id`,`login`,`password`)
 VALUES (1,'user1','0000');
 	  
 INSERT INTO backup(`id`,`date_creation`,`date_last`,`nbj`,`user`)
-VALUES (1,'2016-12-01','2016-12-02',`1`,`1`);
+VALUES (1,'2016-12-01','2016-12-02',1,1);
 
 INSERT INTO `budget` (`id`, `ageTravail`, `ageRetraite`, `chargeSalariale`, `chargeCadre`, `salaireStandard`, `salaireCadre`, `nbSalaries`, `nbCadres`, `nbj`,`backup`) VALUES
 (1, 14, 70, 0, 0, 1000, 1500, 100, 20,1,1);
@@ -16,7 +16,7 @@ INSERT INTO `categorie` (`id`, `libelle`) VALUES
 (4, 'distraction'),
 (5, 'sante');
 
-INSERT INTO `sante` (`id`,`hygiene`,`nbMalades`,`nbAccidents`,`soins`,`echecs`,`nbj`,`backup`) VALUES 
+INSERT INTO `sante` (`id`,`hygiene`,`nbMalades`,`nbAccidents`,`soins`,`echecs`,`nbj`,`backup`) 
 VALUES (1,1,20,2,5,1,1,1);
 
 INSERT INTO `construction`(id,designation,w,h,price,baseSalarie,baseCadre,baseRisque,baseAttractivite,modSalarie,modCadre,modRisque,modAttractivite,specificite,categorie)
@@ -40,14 +40,12 @@ VALUES
 (17,'Caserne',1,1,1000.0,80,20,0,1,0.3,0.1,0,1,'ras',2),
 (18,'Tour_militaire',1,1,1000.0,0,0,0,0,0,0,0,0,'ras',2),
 (19,'usine',1,1,1000.0,0,0,0,0,0,0,0,0,'ras',3),
---ecole
 (20,'universite',1,1,1000.0,40,10,3,1,0.5,0.2,2,1,'ras',1)
 ;
 
 INSERT INTO `education` (`id`,`edTotale`,`edSecurite`,`edEntretien`,`edSante`,`edRecherche`,`edTourisme`,`nbj`,`backup`) 
 VALUES (1,10,10,10,10,10,10,1,1);
---
---spe '{"truc":1}'
+
 INSERT INTO `backup_construction` (`id`, backup,`construction`, `x`, `y`, `nbSalarie`, `nbCadres`, `risque`, `budget`, `attractive`, `postePourvu`, `specificite`) VALUES
 (1,1,1, 1, 1, 0,0,1,1,1,0,'{"truc":1}'),
 (2,1,6, 1, 1, 0,0,1,1,1,0,'{"truc":1}'),
@@ -67,10 +65,8 @@ INSERT INTO `backup_construction` (`id`, backup,`construction`, `x`, `y`, `nbSal
 
 
 INSERT INTO `criminalite` (`id`, `indicMineur`, `crimeMineur`, `indicMoyen`, `crimeMoyen`, `indicGrave`, `crimeGrave`, `indicTerrorisme`, `crimeTerroriste`, `nbj`, `backup`) VALUES
-(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+(1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1);
 
 INSERT INTO `population` (`id`, `fertilite`, `attractivite`,`popTab`,`nbj`,`backup`) VALUES
 (1, 1, 1, '1000',1, 1);
---
-INSERT INTO `user` (`id`, `login`, `password`) VALUES
-(1, 'user1', '0000');
+
